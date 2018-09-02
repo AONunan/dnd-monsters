90b66ac35b61a3b7daed0b15dea812cbbb4e0c6e
Alan Noonan <aganoonan@gmail.com>
2018-09-02 18:27:48 +0100

    Display added monsters

---
+++ src/MonsterDetails.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MonsterDetails = (props) => {
+    return (
+        <p>Added monsters: {props.addedMonsters.toString()}</p>
+    );
+}
+
+export default MonsterDetails;
