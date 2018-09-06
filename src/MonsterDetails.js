d734d5bbb17e73a9e4c009a4b9859efd8673ddf7
Alan Noonan <aganoonan@gmail.com>
2018-09-06 20:42:40 +0100

    Populate MonsterDetails table

--- src/MonsterDetails.js
+++ src/MonsterDetails.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import monstersJson from './Data/5e-SRD-Monsters.json'
 
 const MonsterDetails = (props) => {
+    let rows = props.addedMonsters.map((monsterName, index) => {
+
+        let monster = monstersJson.find((object) => object.name === monsterName);
+        // console.log("monster:", monster);
+
+        return (
+            <tr key={index}>
+                <td>{monster.name}</td>
+                <td>{monster.type}</td>
+                <td>{monster.hit_points}</td>
+                <td>{monster.actions[0].name}</td>
+            </tr>
+        )
+    });
+
     return (
-        <p>Added monsters: {props.addedMonsters.toString()}</p>
+        <table>
+        <thead>
+        <tr>
+            <th>Name</th>
+            <th>Basic Info</th>
+            <th>Stats</th>
+            <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+        </table>
     );
 }
 
